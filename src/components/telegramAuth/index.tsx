078eed754624a5a79d35abc7ccd37132c8c5424a
4iTeRiNo <zubkobigot@mail.ru>
2024-09-11 14:50:32 +0200

    [*] fix setting telegram

--- src/components/telegramAuth/index.tsx
+++ src/components/telegramAuth/index.tsx
@@ -6,9 +6,9 @@ const TelegramAuthWidget = () => {
       <script
         async
         src="https://telegram.org/js/telegram-widget.js?7"
-        data-telegram-login={process.env.BOT_USERNAME}
+        data-telegram-login="zoorama_bot"
         data-size="large"
-        data-auth-url={`${process.env.BASE_URL}/animal-card/1`}
+        data-auth-url="https://landing-zoo-mu.vercel.app/auth/login"
         data-request-access="write"
         data-on-auth={`${handler}`}
       />
