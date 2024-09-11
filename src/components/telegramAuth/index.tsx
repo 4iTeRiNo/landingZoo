e4cc3f3c9b47158964cc7f703a254efab968f6db
4iTeRiNo <zubkobigot@mail.ru>
2024-09-11 15:00:23 +0200

    [*] update setting

--- src/components/telegramAuth/index.tsx
+++ src/components/telegramAuth/index.tsx
@@ -2,16 +2,16 @@ import handler from "@/app/lib/callback";
 
 const TelegramAuthWidget = () => {
   return (
-    <div>
+    <div className="text-white">
       <script
         async
-        src="https://telegram.org/js/telegram-widget.js?7"
-        data-telegram-login="zoorama_bot"
+        src="https://telegram.org/js/telegram-widget.js?22"
+        data-telegram-login="@zoorama_bot"
         data-size="large"
-        data-auth-url="https://landing-zoo-mu.vercel.app/auth/login"
+        data-auth-url="http://localhost:3000/animal-card/1"
         data-request-access="write"
-        data-on-auth={`${handler}`}
       />
+      Войти
     </div>
   );
 };
